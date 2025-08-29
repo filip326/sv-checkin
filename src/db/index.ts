10f7172b46164a3d4a8f3fb6313d44ff4b17a9bf
heeecker <89743412+filip326@users.noreply.github.com>
2025-08-29 20:04:15 +0200

    implement expired token deletion

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,28 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import * as schema from "./schema";
+import { eq } from "drizzle-orm";
+import { fromISOTimestamp } from "../utils/dates";
 
 const db = drizzle({
-  connection: {
-    source: process.env.DB_FILENAME!,
-  },
+    connection: {
+        source: process.env.DB_FILENAME!,
+    },
 
-  schema: {
-    ...schema,
-  }
-
+    schema: {
+        ...schema,
+    },
 });
+
+// tokens shall be deleted when expired
+setInterval(async () => {
+    const existingTokens = await db.query.tokens.findMany({ columns: { id: true, expiresAt: true } });
+
+    const now = new Date();
+    for (const token of existingTokens) {
+        if (fromISOTimestamp(token.expiresAt) < now) {
+          db.delete(schema.tokens).where(eq(schema.tokens.id, token.id));
+        }
+    }
+}, 15 * 60 * 1_000); // one hour
 
 export default db;
